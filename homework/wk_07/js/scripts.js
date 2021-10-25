028faae1a6ce1e11be133f12b56092e56ebcc908
Gabe Deko <gabe.deko@gmail.com>
2021-10-24 21:30:48 -0400

    clean up code

--- homework/wk_07/js/scripts.js
+++ homework/wk_07/js/scripts.js
@@ -1,83 +1,17 @@
-/////////////////
-////////////////
-// Callback Fetch
-/*
-let button0 = document.getElementById('GetUsersCallback');
 
-button0.addEventListener("click", getUserData);
-
-function getUserData(e) {
-  e.preventDefault();
-
-  const url = "https://randomuser.me/api/?results=1";
-  const url2 = "http://api.open-notify.org/iss-now.json";
-
-  fetch(url)
-    .then(function(response) {
-        return response.json();
-    })
-    .then( function(data) {
-        console.log('raw data',data);
-        document.getElementById("Output").innerHTML = JSON.stringify(data.results[0]);
-        coords = data.results[0].location.coordinates;
-
-       return fetch(url2);
-    } )
-    .then( function(response) {
-        return response.json();
-    })
-    .then(function(resp) {
-        console.log('raw data',resp);
-        let iss = resp.iss_position;
-        document.getElementById("Output").innerHTML += `<br>ISS position is: ${iss.latitude} / ${iss.longitude}`;
-        let dist = distance( coords.latitude, coords.longitude, iss.latitude, iss.longitude);
-        document.getElementById("Output").innerHTML += `<br>Current distance between the two  is: ${dist} miles`;
-    })
-    .catch(function(error) {
-        document.getElementById("Output").innerHTML = "There was an error "+error;
-    });
-}
-*/
-
-/////////////////
-////////////////
-// 10. Fetch GET
-/*
-let button1 = document.getElementById('GetUsers');
-button1.addEventListener("click", getUserData);
-
-function getUserData() {
-  let url = "https://reqres.in/api/users";
-    fetch(url)
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(resp) {
-        console.log(resp);
-        document.getElementById("Output").innerHTML = JSON.stringify(resp.data);
-      })
-      .catch(function(resp) {
-        document.getElementById("Output").innerHTML = "There was an error";
-      });
-}
-*/
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Spotify Fetch Get
 
-//const form = document.getElementById('createUser');
+// set form as variable
 const form = document.getElementById('getSpotifyPlaylists');
 
-
-
-//let button2 = document.getElementById('getSoundCloudData');
-//button2.addEventListener("click", getSoundcloudData);
-
+// API controller for handling calls
 const APIController = (function(){
-  const clientId = '';
-  const clientSecret = '';
 
+  // ID and Secret removed for security purposes
+  const clientId = '********';
+  const clientSecret = '********';
+
+  // call for Spotify token authentication
   const _getSpotifyTokens = async () => {
 
       let url = "https://accounts.spotify.com/api/token";
@@ -99,18 +33,8 @@ const APIController = (function(){
           return data.access_token;
 
   }
-  /*
-  const _getGenres = async (token) => {
 
-    const result = await fetch(`https://api.spotify.com/v1/browse/categories?locale=sv_US`, {
-        method: 'GET',
-        headers: { 'Authorization' : 'Bearer ' + token}
-    });
-    
-    const data = await result.json();
-    return data;
-  }
-  */
+  // callf or getting user's playlists
   const _getPlaylists = async (token, userId) => {
 
     let _userId = userId;
@@ -124,37 +48,33 @@ const APIController = (function(){
     return data;
   }
 
+  // returns GLOBAL functions for GLOBAL use
   return {
     getSpotifyToken() {
       return _getSpotifyTokens();
     },
-    /*
-    getGenres(token) {
-      return _getGenres(token);
-    },
-    */
+
     getPlayLists(token, userId) {
       return _getPlaylists(token, userId);
     }
   }
 })();
 
+// UI controller for handling what content/media appears in app
 const UIController = (function() {
 
     //object to hold references to html selectors
     const DOMElements = {
       selectGenre: '#select_genre',
       getPlaylist: '#get_playlist',
-      // selectPlaylist: '#select_playlist',
       buttonSubmit: '#btn_submit',
-      // divSongDetail: '#song-detail',
       hfToken: '#hidden_token',
-      // divSonglist: '.song-list'
     }
 
     //public methods
     return {
 
+      // set DOM elements as variables
       inputField() {
         return {
           genre: document.querySelector(DOMElements.selectGenre),
@@ -162,17 +82,13 @@ const UIController = (function() {
           submit: document.querySelector(DOMElements.buttonSubmit),
         }
       },
-      // need methods to create select list option
-      /*
-      createGenre(text, value) {
-        const html = `<option value="${value}">${text}</option>`;
-        document.querySelector(DOMElements.selectGenre).insertAdjacentHTML('beforeend', html);
-      },
-      */
+ 
       createPlaylist(name, link, image, total) {
         //console.log(image);
 
-        if( image.length == 0 || image === undefined ) {
+
+        // generate playlist items with data
+        if( image.length == 0 || image === undefined ) { // if playlist art does not exist use default image
 
           const html = `<a href="${link}" target="_blank" class="playlist-item-container" style="background-image:url(imgs/records.jpg);">`
           + `<div class="playlist-item">`
@@ -196,9 +112,11 @@ const UIController = (function() {
         }
         
       },
+      //stores authorization token
       storeToken(value) {
         document.querySelector(DOMElements.hfToken).value = value;
       },
+      // retrieves stored token
       getStoredToken() {
         return {
             token: document.querySelector(DOMElements.hfToken).value
@@ -212,28 +130,7 @@ const APPController = (function(UICtrl, APICtrl) {
   // get input field object ref
   const DOMInputs = UICtrl.inputField();
 
-  /*
-  const loadGenres = async () => {
-    //get token
-    ;
-    const token = await APICtrl.getSpotifyToken();
-    
-    //store token onto page
-    UICtrl.storeToken(token);
-    //get the genres
-    const genres = await APICtrl.getGenres(token);
-    //console.log("genres ", genres.categories.items);
-    //populate genre select element 
-    
-    // create genre for each item in JSON
-    genres.categories.items.forEach(element => UICtrl.createGenre(element.name, element.id));
-    // for (var i = 0; i < genres.categories.items.length; i++){
-    //   console.log("wow");
-    //   //UICtrl.createGenre(element.name, element.id)
-    // }
-  }
-  */
-
+  // when button is clicked trigger playlist generation
   DOMInputs.submit.addEventListener('click', async (e) => {
     // prevent page reset
     e.preventDefault();
@@ -253,16 +150,13 @@ const APPController = (function(UICtrl, APICtrl) {
     playlist.items.forEach(element => UICtrl.createPlaylist(element.name, element.external_urls.spotify, element.images, element.tracks.total))
   });
 
+  // declares app has sarted
   return {
     init() {
       console.log('App is starting');
-      //Genres();
     }
   }
   
-})(UIController, APIController);
+})(UIController, APIController); // app controller uses UI and API controllers
 
-APPController.init();
-
-
-// /https://api.spotify.com/v1/users/{user_id}
+APPController.init(); // initializes app
